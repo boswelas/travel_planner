494f1b9161294a1e531d0e1c79d1b0b7e349fba6
Conner <conner.marchell@gmail.com>
2023-04-16 16:05:10 -0500

    Added mysql database connection

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from flask import Flask, jsonify
 import os
+import mysql.connector
 
 app = Flask(__name__)
 
+HOST = os.environ['MYSQLHOST']
+PORT = os.environ['MYSQLPORT']
+USER = os.environ['MYSQLUSER']
+PASSWORD = os.environ['MYSQLPASSWORD']
+DATABASE = os.environ['MYSQLDATABASE']
+
+db = mysql.connector.connect(
+    host=HOST,
+    port=PORT,
+    user=USER,
+    password=PASSWORD,
+    database=DATABASE
+)
 
 @app.route('/')
 def index():
