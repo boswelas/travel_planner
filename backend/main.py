647f344d78774cd5f5c6cf7942a16325c5087237
Conner <conner.marchell@gmail.com>
2023-04-15 20:52:42 -0500

    Updated greeting of root directory

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify("Welcome to the Travel Planner backend!")
 
 
 if __name__ == '__main__':
