295b0b38ead496fb0cde09f97be04eaffaf4cd9f
Conner <conner.marchell@gmail.com>
2023-04-16 16:05:46 -0500

    Added GET for all users

--- backend/main.py
+++ backend/main.py
@@ -23,5 +23,16 @@ def index():
     return jsonify("Welcome to the Travel Planner backend!")
 
 
+@app.route('/user')
+def get_all_users():
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM user")
+    result = cursor.fetchall()
+    print(result)
+
+    return jsonify(f"database name: {result}")
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
