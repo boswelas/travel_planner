fb9523c33150df2a0c088f20217e023ac192a42c
Conner <conner.marchell@gmail.com>
2023-04-17 21:17:09 -0500

    Added example of pulling data from backend

--- frontend/src/components/user-table.js
+++ frontend/src/components/user-table.js
@@ -2,6 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 export default function UserTable() {
 
+    const [data, setData] = useState([]);
+
+    async function GetUserData() {
+        const response = await fetch("http://travel-planner-production.up.railway.app/user");
+        const jsonData = await response.json();
+        setData(jsonData.result)
+      }
+
+    const listRows = data.map((row) =>
+      <tr>
+          <td>{row[0]}</td>
+          <td>{row[1]}</td>
+          <td>{row[2]}</td>
+          <td>{row[3]}</td>
+      </tr>
+  )
+
+    useEffect(() => {
+        GetUserData();
+
+    }, [])
+
     return (
         <table>
             <thead>
@@ -9,14 +31,11 @@ export default function UserTable() {
                     <th>ID</th>
                     <th>First Name</th>
                     <th>Last Name</th>
+                    <th>Birth Day</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>99</td>
-                    <td>Bobby</td>
-                    <td>Hill</td>
-                </tr>
+                {listRows}
             </tbody>
         </table>
     )
