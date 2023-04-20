b5026dbb9bcf6eb9de7d86f275620d73b0832876
Victoria Patterson <67242554+vrpatterson@users.noreply.github.com>
2023-04-20 10:39:16 -0700

    Added routes for Experiences

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect, render_template
 from flask_cors import CORS
 import os
 import mysql.connector
@@ -46,6 +46,46 @@ def get_all_users():
     cnx.close()
 
     return jsonify(result=result)
+
+############################# BEGIN route for Experiences #############################
+@app.route("/experiences", methods=["POST", "GET"])
+def experiences():
+    # Insert new experience
+    if request.method == "POST":
+        if request.form.get("add_experience"):
+            # Grab experience form inputs
+            title = request.form["title"]
+            description = request.form["description"]
+            geo_location = request.form["geo_location"]
+            image = request.form["image"]   # probably not correct syntax!!!
+            keywords = request.form["keywords"]
+
+            # Add data
+            query = "INSERT INTO Experiences (title, description, geo_location, image, keywords) VALUES (%s, %s, %s, %s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (title, description, geo_location, image, keywords))
+            mysql.connection.commit()
+
+            # Redirect back to experiences
+            return redirect("/experiences")
+        
+    # Display experiences using query to grab all experiences in Experiences
+    if request.method == "GET":
+        search_query = request.query_string.decode()
+        if search_query:
+            query = f"SELECT * FROM Experiences WHERE MATCH (`title`, `description`, `geo_location`, `image`, `keywords`) AGAINST ('{search_query[2:]}' IN NATURAL LANGUAGE MODE);"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            data = cur.fetchall()
+        else:
+            query1 = "SELECT title, description, geo-location, image, keywords FROM experiences"
+            cur = mysql.connection.cursor()
+            cur.execute(query1)
+            data = cur.fetchall()
+        return render_template("experiences.j2", data=data)
+    
+
+############################# END route for Experiences #############################
 
 
 if __name__ == '__main__':
