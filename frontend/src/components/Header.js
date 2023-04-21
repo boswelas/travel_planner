2532ddbffb1586b8ffb42521c7c69019da433d81
Conner <conner.marchell@gmail.com>
2023-04-21 08:14:47 -0500

    Added rough pass of styling for header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,17 @@
 import Link from "next/link";
+import styles from '../styles/Nav.module.css'
 
 const Header = () => {
     return ( 
-        <nav>
-            <div>
-                <Link href="/"><h1 className="logo">Travel Planner</h1></Link>
-            </div>
-            <Link href="/Trips">My Trips</Link>
-            <Link href="/explore">Explore</Link>
-            <Link href="/login">Login</Link>
-            <Link href="/signup">Signup</Link>
-        </nav>
+        <header>
+            <nav class={styles.header}>
+                <Link href="/"><div className={styles.logo}>Travel Planner</div></Link>
+                <Link href="/Trips">My Trips</Link>
+                <Link href="/explore">Explore</Link>
+                <Link href="/login">Login</Link>
+                <Link href="/signup">Signup</Link>
+            </nav>
+        </header>
      );
 }
  
