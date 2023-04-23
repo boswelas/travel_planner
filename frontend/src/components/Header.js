8742196edba1012c888047b73dd5c064791da373
Conner <conner.marchell@gmail.com>
2023-04-23 10:31:40 -0500

    Added search bar to Header bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from '../styles/Nav.module.css'
+import SearchBar from "./SearchBar";
 
 const Header = () => {
     return ( 
@@ -8,6 +9,7 @@ const Header = () => {
                 <Link href="/"><div className={styles.logo}>Travel Planner</div></Link>
                 <Link href="/trips">My Trips</Link>
                 <Link href="/experience">Explore</Link>
+                <SearchBar />
                 <Link href="/login">Login</Link>
                 <Link href="/signup">Signup</Link>
             </nav>
