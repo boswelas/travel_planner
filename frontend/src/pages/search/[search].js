c1c2be4360b0269150cc865bf727527da8274aca
Conner <conner.marchell@gmail.com>
2023-04-23 13:53:11 -0500

    Converted search page to use url params.

--- frontend/src/pages/search/[search].js
+++
@@ -1,7 +0,0 @@
-const Search = () => {
-    return ( 
-        <h3>This is the search page</h3>
-     );
-}
- 
-export default Search;
---
+++ frontend/src/pages/search/index.js
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+import SearchBar from "@/components/SearchBar";
+
+const Search = () => {
+
+}
+ 
+export default Search;
