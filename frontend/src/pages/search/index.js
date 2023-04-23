a0d7105a1d5a0bfb54c6a67567974749244fb18c
Conner <conner.marchell@gmail.com>
2023-04-23 13:53:33 -0500

    Setup basic search page structure

--- frontend/src/pages/search/index.js
+++ frontend/src/pages/search/index.js
@@ -3,6 +3,36 @@ import SearchBar from "@/components/SearchBar";
 
 const Search = () => {
 
+    const router = useRouter()
+    const query = router.query
+
+    const core = (query) => {
+
+        if (typeof query.search == 'string') {
+            // User has input a search
+            return (<>
+                        <p>Results for {query.search}:</p>
+                    </>
+            )
+        } else {
+            // No search params were input
+            return (<>
+                        <p>Search for an experience below!</p>
+                        <SearchBar />
+                    </>
+            )
+        }
+    }
+
+
+    return (
+        <>
+            <h3>Search Page</h3>
+
+            {core(query)}
+
+        </>
+     );
 }
  
 export default Search;
