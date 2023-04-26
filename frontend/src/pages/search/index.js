97c99a2aebe3357ff8263db2e2cd835ce7add903
Conner <conner.marchell@gmail.com>
2023-04-26 07:22:06 -0500

    First working pass of search

--- frontend/src/pages/search/index.js
+++ frontend/src/pages/search/index.js
@@ -1,17 +1,74 @@
 import { useRouter } from "next/router";
 import SearchBar from "@/components/SearchBar";
+import React, { useState, useEffect } from 'react';
+
 
 const Search = () => {
 
     const router = useRouter()
     const query = router.query
 
-    const core = (query) => {
+    const [data, setData] = useState([]);
+    const [rows, setRows] = useState([]);
+
+    const PerformSearch = async (query) => {
+
+        const res = await fetch(`http://127.0.0.1:5000/search?search=${query}`)
+        const data = await res.json();
+
+        console.log(`URL: http://127.0.0.1:5000/search?search=${query}`)
+        console.log(res)
+        console.log(data)
+        setData(data)
+        return data
+    }
+
+    const genTable = (dataArrays) => {
+
+        console.log(`Type of data ${typeof dataArrays}`)
+        console.log(`dataArrays: ${dataArrays}`)
+
+        const listRows = dataArrays.map((row, index) =>
+            <tr key={index}>
+                <td>{row[1]}</td>
+                <td>{row[2]}</td>
+                <td>{row[3]}</td>
+                <td>{row[4]}</td>
+            </tr>
+        )
+
+        setRows(listRows)
+    }
+
+    useEffect(() => {
+        if(!router.isReady) return;
+
+        PerformSearch(query['search'])
+        .then((res) => genTable(res))
+
+    }, [router.isReady])
+    
+
+    const core = () => {
 
         if (typeof query.search == 'string') {
             // User has input a search
             return (<>
                         <p>Results for {query.search}:</p>
+                        <table>
+                        <thead>
+                            <tr>
+                                <th>Experience</th>
+                                <th>City</th>
+                                <th>State</th>
+                                <th>Country</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rows}
+                        </tbody>
+                    </table>
+                        
                     </>
             )
         } else {
@@ -23,7 +80,6 @@ const Search = () => {
             )
         }
     }
-
 
     return (
         <>
