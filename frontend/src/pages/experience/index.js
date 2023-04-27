24ed80bca240498482fcf5df4ff6c08a4ec39196
Victoria <vrpatter@gmail.com>
2023-04-27 09:44:01 -0700

    added link to experience table

--- frontend/src/pages/experience/index.js
+++ frontend/src/pages/experience/index.js
@@ -1,11 +1,17 @@
 import Link from 'next/link'
+import ExperienceTable from '@/components/experience-table'
+
+// export const getStaticProps = async () => {
+
+// }
 
 const Experience = () => {
     return (
-        <div>
+        <><div>
             <h1>Welcome to the Experience default</h1>
+            <ExperienceTable />
             <Link href="/experience/addNewExperience">Add New Experience</Link>
-        </div>
+        </div></>
 
     )
 };
