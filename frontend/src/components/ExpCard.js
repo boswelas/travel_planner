99a2f6c5038c3419ea41cc410908bf2d4ac5de93
Ashley Boswell <boswelas@oregonstate.edu>
2023-05-04 10:21:20 +0900

    corrected api for trip queries

--- frontend/src/components/ExpCard.js
+++ frontend/src/components/ExpCard.js
@@ -10,7 +10,7 @@ const ExpCard = ({ props, showViewMore = true, fromTrip, trip_id }) => {
     const handleDeleteFromTrip = async (event) => {
         event.preventDefault();
         try {
-            const response = await fetch('http://127.0.0.1:5001/deleteExperienceFromTrip', {
+            const response = await fetch('https://travel-planner-production.up.railway.app/deleteExperienceFromTrip', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
