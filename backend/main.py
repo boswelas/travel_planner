ffe3de0d3b08a23c51b7f100b77c3a947bc2fbd2
Conner <conner.marchell@gmail.com>
2023-05-06 18:36:57 -0500

    Changed id to experience_id to flow through cards

--- backend/main.py
+++ backend/main.py
@@ -212,7 +212,7 @@ def search():
         # Convert the data to a dictionary so that it shows up as an object
         payload = []
         for row in data:
-            payload.append({'id': row[0], 'title': row[1], 'city': row[2], 'state': row[3],
+            payload.append({'experience_id': row[0], 'title': row[1], 'city': row[2], 'state': row[3],
                            'country': row[4], 'rating': row[5], 'description': row[6]})
 
         return jsonify(payload)
@@ -456,7 +456,7 @@ def LatestExp():
         # Convert the data to a dictionary so that it shows up as an object
         payload = []
         for row in data:
-            payload.append({'id': row[0], 'title': row[1], 'city': row[2], 'state': row[3],
+            payload.append({'experience_id': row[0], 'title': row[1], 'city': row[2], 'state': row[3],
                            'country': row[4], 'rating': row[5], 'description': row[6]})
 
         return jsonify(payload)
