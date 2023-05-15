3502b23107d6dfa145432e360376301b6277893a
Ashley Boswell <boswelas@oregonstate.edu>
2023-05-15 18:40:58 +0900

    corrected api

--- frontend/src/components/getUserRating.js
+++ frontend/src/components/getUserRating.js
@@ -2,24 +2,24 @@
 
 export const handleGetRating = async (getToken, experience_id) => {
 
-  const token = await getToken();
+    const token = await getToken();
 
-  try {
-    const response = await fetch('http://127.0.0.1:5001/getUserRating', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        experience_id,
-      }),
-    });
+    try {
+        const response = await fetch('https://travel-planner-production.up.railway.app/getUserRating', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                experience_id,
+            }),
+        });
 
-    const data = await response.json();
-    return data.rating;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+        const data = await response.json();
+        return data.rating;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
